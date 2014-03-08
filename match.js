4bff4c1adc271399222c08f8b0d2b9e52ea74934
Evan Sharp <dumbdrum@gmail.com>
2014-03-07 23:11:18 -0500

    Got rid of $parse
    Not sure why I ever did it that way... Seems to work just the same with the scope set

--- match.js
+++ match.js
@@ -1,14 +1,17 @@
     var app = angular.module('myApp', []);
-    app.directive('match',['$parse', function ($parse) {
+    app.directive('match', function () {
         return {
             require: 'ngModel',
             restrict: 'A',
+            scope: {
+                match: '='
+            },
             link: function(scope, elem, attrs, ctrl) {
                 scope.$watch(function() {
-                    return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || $parse(attrs.match)(scope) === ctrl.$modelValue;
+                    return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
                 }, function(currentValue) {
                     ctrl.$setValidity('match', currentValue);
                 });
             }
         };
-    }]);
+    });
